4afd9bc9ef1fdf1175d2fd448fa1a041af94cfbf
Welder N. Fernandes <welderx3@gmail.com>
2025-05-29 20:03:41 -0300

    feat: adicionar botão de deslogar na página do dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,23 @@
-export default function DashboardPage() {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import SignOutButton from "./components/sign-out-button";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
+      <h1>{session?.user?.name}</h1>
+      <h1>{session?.user?.email}</h1>
+      <SignOutButton />
     </div>
   );
 }
